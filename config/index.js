e54d49a33abb2e8f66fef5f5be60f89bd3f929f9
kuishang <kuishang@outlook.com>
2018-03-23 17:09:01 +0800

    for pro

--- config/index.js
+++ config/index.js
@@ -1,7 +1,15 @@
+
+let url
+if (process.env.NODE_ENV === 'production') {
+	url = 'mongodb://kk_blog:kk_123@mongodb/blog'
+} else {
+	url = 'mongodb://kk_blog:kk_123@localhost:27017/blog'
+}
+
 module.exports = {
 	port: 9528,
 	// url: 'mongodb://kk_blog:kk_123@localhost:27017/blog',
-	url: 'mongodb://kk_blog:kk_123@ddb/blog',
+	url: url,
 	// url: 'mongodb://localhost:27017/blog',
 	session: {
 		name: 'SID',
